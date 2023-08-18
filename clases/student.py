d88034ea36a6af3c4b8328634d909f2fb932581e
Jpenuela420 <penuelaherrerajuanjose@gmail.com>
2023-08-18 11:30:39 -0500

    creé la clase Student

--- clases/student.py
+++ clases/student.py
@@ -1,2 +1,10 @@
 class Student:
-    def __init__(self):
+    def __init__(self, name:str,age:int):
+        self.name = name
+        self.age = age
+        self.grade = []
+
+    def add_grade(self, num:float):
+        (self.grade).append(num)
+
+
